acc9897c6f62fdc5d362bd808d32e4f47169cef2
Dylan Fierro <fierrodylan34@gmail.com>
2022-09-25 00:41:47 -0600

    add script

---
+++ dotMaker.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+DIR2GIT="$HOME/Downloads/repos/Dotfiles/"
+
+sudo cp -r "$HOME/.config/waybar" "$DIR2GIT"
+sudo cp -r "$HOME/.config/hypr/" "$DIR2GIT"
+sudo cp -r "$HOME/.config/BetterDiscord/themes/" "$DIR2GIT"
+sudo cp -r "$HOME/.config/wezterm/" "$DIR2GIT"
+sudo cp -r "$HOME/.config/rofi/" "$DIR2GIT"
+sudo cp "$HOME/.zshrc" "$DIR2GIT"
+sudo cp "$HOME/.config/starship.toml" "$DIR2GIT"
